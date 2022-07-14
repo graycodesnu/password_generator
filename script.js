0a9544495510ab660ceedee3701b199fdd591235
graycodesnu <graysonharvey@graysons-mbp.lan>
2022-07-13 22:40:01 -0500

    added generator functions, beginning DOM element assignments.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
+
+//elements 
 var generateBtn = document.querySelector("#generate");
+
+const password = document.querySelector("#password");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +15,23 @@ function writePassword() {
 
 }
 
+//Generator functions for characters 
+
 // function getRandomLower() {
 //   return.String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+
+// function getRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+// }
+
+// function getRandomNumber() {
+//  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+//  }
+
+// function getRandomSymbol() {
+//   const symbols = "!@#$%^&*(){}[]=<>/,.";
+//   return symbols[Math.floor(Math.random() * symbols.length];
 // }
 
 // Add event listener to generate button
