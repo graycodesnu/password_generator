a445c51368fc8764ce4d2c0dfa522f936538b62e
graycodesnu <graysonharvey@graysons-mbp.lan>
2022-07-13 21:17:30 -0500

    pseudo code for randomized lowercase letters, made generatePassword equal an empty string.

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword("");
   var passwordText = document.querySelector("#password");
-
+    
   passwordText.value = password;
 
 }
+
+// function getRandomLower() {
+//   return.String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
