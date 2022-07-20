137d1ec39982949030e0315d8f3eaec4512ef078
graycodesnu <graysonharvey@Graysons-MacBook-Pro.local>
2022-07-20 10:34:04 -0500

    added if/else, else/if statements to the password length and uppercase checkbox elements. need to figure out how to randomize characters and have all criteria populate to password element.

--- script.js
+++ script.js
@@ -3,107 +3,54 @@
 //elements
 var generateBtn = document.querySelector("#generate");
 
-var password = document.querySelector("#password");
-var lengthInputEl = document.querySelector("#inputs");
+var passwordTextEl = document.querySelector("#password");
+var lengthInputEl = document.querySelector("#length");
 var uppercaseCheckBoxEl = document.querySelector("#uppercase");
 var lowercaseCheckBoxEl = document.querySelector("#lowercase");
 var numbersCheckBoxEl = document.querySelector("#numbers");
 var symbolsCheckBoxEl = document.querySelector("#symbols");
 
-var validCharacters = "abcdefghijklmnopqrstuvwxyz";
+// passwordTextEl.innerHTML = "this is a string";
 
-var password = "";
+// Define valid characters
+var validCharacters;
+var getUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var getLowercase = "abcdefghijklmnopqrstuvwxyz";
+var getSymbols = "!@#$%^&*()";
+var getNumbers = "0123456789";
 
-while (password.length < passwordLength){
+// Link to selected (or unselected) checkboxes
 
-var randomIndex = Math.floor(Math.random() * validCharacters.length);
-var randomCharacter = validCharacters[randomIndex];
-  password += randomCharacter;
-
-for (var i = 0; i < passwordLength; i ++){}
-
-
-
-if (includeLowercase) = "" {
-  validCharacters += "123456789".getRandomNumber
-}
-
-
-function writePassword() {
-  var length = Number.parseInt(lengthInputEl.value, 10);
-  var hasUpper = uppercaseCheckBoxEl.ariaChecked;
-  var hasLower = lowercaseCheckBoxEl.ariaChecked;
-  var hasNumbers = numbersCheckBoxEl.ariaChecked;
-  var hasSymbols = symbolsCheckBoxEl.ariaChecked;
-
-//Define + call selectors function
-// var selectors = {
-//   lower: getRandomLower,
-//   upper: getRandomUpper,
-//   number: getRandomNumber,
-//   symbols: getRandomSymbol,
-// };
+// Link criteria functions to password field/function
 
 // Write password to the #password input
-function generatePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-  //selectors function
-  var typesCount = lower + upper + number + numbers + symbols;
-
-  var typesArr = [{ lower }, { upper }, { numbers }, { symbols }].filter(
-    (item) => Object.values(item)[0]
-  );
-
-  //no checkboxes selected
-  if (typesCount === 0) {
-    return "";
+function writePassword() {
+  console.log("hi");
+  //var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  console.log(lengthInputEl.value);
+  var lengthInputValue = lengthInputEl.value;
+    if (lengthInputValue === 128) {
+    console.log("yay!");
+    } else {
+    console.log("no");
   }
 
-  //loop
-  // for (let i = 0; i < length; i += typesCount) {
-  //   typesArr.forEach((type) => {
-  //     var funcName = Object.keys(type)[0];
-  //     writePassword += randomGen[funcName]();
-  //   });
-  // }
+  passwordText.value = password;
 
-  var generatePassword = writePassword.slice(0, length);
-  return password;
+  // Uppercase function
+    var getUppercase = uppercaseCheckBoxEl
+    // Maybe write: uppercaseRandom ( randomize?)  
+      if (getUppercase = true) {
+      console.log('get uppercase');
+      } else if (getUppercase = false) {
+        console.log('no uppercase');
+      }
+
+
+      
+  // Select el >>> .innerHTML = value
 }
-
-//Generator functions for characters
-
-// function getRandomLower() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-
-// function getRandomUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-
-// function getRandomNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
-
-// function getRandomSymbol() {
-//   const symbols = "!@#$%^&*(){}[]=<>/,.";
-//   return symbols[Math.floor(Math.random() * symbols.length)];
-// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-  password.innerText = writePassword(
-    length,
-    hasUpper,
-    hasLower,
-    hasNumbers,
-    hasSymbols
-  );
-}
