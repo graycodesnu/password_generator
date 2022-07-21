5f1608db2c874200273d9ca9870c3e4e9ea4e550
graycodesnu <graysonharvey@graysons-mbp.lan>
2022-07-21 17:35:41 -0500

    added logic for uppercase function.

--- script.js
+++ script.js
@@ -4,118 +4,59 @@
 var generateBtn = document.querySelector("#generate");
 
 var passwordTextEl = document.querySelector("#password");
-var lengthInputEl = document.querySelector("#input");
+var lengthInputEl = document.querySelector("#length");
 var uppercaseCheckBoxEl = document.querySelector("#uppercase");
 var lowercaseCheckBoxEl = document.querySelector("#lowercase");
 var numbersCheckBoxEl = document.querySelector("#numbers");
 var symbolsCheckBoxEl = document.querySelector("#symbols");
 
-var validCharacters = "abcdefghijklmnopqrstuvwxyz";
+// passwordTextEl.innerHTML = "this is a string";
+
+// Define valid characters
+var validCharacters;
+var getUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var getLowercase = "abcdefghijklmnopqrstuvwxyz";
+var getSymbols = "!@#$%^&*()";
+var getNumbers = "0123456789";
+
+// Link to selected (or unselected) checkboxes
+
+// Link criteria functions to password field/function
 
 // Write password to the #password input
 function writePassword() {
-  var passwordTextEl = document.querySelector("#password");
-  if (passwordTextEl == null) {
-    return;
+  console.log("hi");
+  //var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  console.log(lengthInputEl.value);
+  var lengthInputValue = lengthInputEl.value;
+    if (lengthInputValue === 128) {
+    console.log("yay!");
+    } else {
+    console.log("no");
   }
-  passwordTextEl.value = generatePassword();
 
-var password = "";
+  passwordText.value = password;
 
-while (password.length < passwordLength){
-  var randomIndex = Math.floor(Math.random() * validCharacters.length);
-  var randomCharacter = validCharacters[randomIndex];
-  password += randomCharacter;
-  }
-  return password
+  // Uppercase function
+    var getUppercase = uppercaseCheckBoxEl
+    // Maybe write: uppercaseRandom ( randomize?)  
+      var uppercaseCheckBoxEl = document.getElementById('uppercase');
+      if(uppercaseCheckBoxEl.checked) {
+        console.log('yes uppercase');
+      } else {
+        console.log('no uppercase');
+      }
+      // if (getUppercase.checked) {
+      // console.log('get uppercase');
+      // } else if (getUppercase = false) {
+      //   console.log('no uppercase');
+      // }
+
+
+      
+  // Select el >>> .innerHTML = value
 }
-
-for (var i = 0; i < passwordLength; i ++){}
-
-
-
-if (includeLowercase) = "" {
-  validCharacters += "123456789".getRandomNumber
-}
-
-
-function writePassword() {
-  var length = Number.parseInt(lengthInputEl.value, 10);
-  var hasUpper = uppercaseCheckBoxEl.ariaChecked;
-  var hasLower = lowercaseCheckBoxEl.ariaChecked;
-  var hasNumbers = numbersCheckBoxEl.ariaChecked;
-  var hasSymbols = symbolsCheckBoxEl.ariaChecked;
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-//Define + call selectors function
-// var selectors = {
-//   lower: getRandomLower,
-//   upper: getRandomUpper,
-//   number: getRandomNumber,
-//   symbols: getRandomSymbol,
-// };
-
-
-//   // passwordText.value = password;
-
-//   //selectors function
-//   var typesCount = lower + upper + number + numbers + symbols;
-
-//   var typesArr = [{ lower }, { upper }, { numbers }, { symbols }].filter(
-//     (item) => Object.values(item)[0]
-//   );
-
-//   //no checkboxes selected
-//   if (typesCount === 0) {
-//     return "";
-  }
-
-  //loop
-  // for (let i = 0; i < length; i += typesCount) {
-  //   typesArr.forEach((type) => {
-  //     var funcName = Object.keys(type)[0];
-  //     writePassword += randomGen[funcName]();
-  //   });
-  // }
-
-//   var generatePassword = writePassword.slice(0, length);
-//   return password;
-// }
-
-//Generator functions for characters
-
-// function getRandomLower() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-
-// function getRandomUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-
-// function getRandomNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
-
-// function getRandomSymbol() {
-//   const symbols = "!@#$%^&*(){}[]=<>/,.";
-//   return symbols[Math.floor(Math.random() * symbols.length)];
-// }
-
-
-
-//   password.innerText = writePassword(
-//     length,
-//     hasUpper,
-//     hasLower,
-//     hasNumbers,
-//     hasSymbols
-//   );
-// }
