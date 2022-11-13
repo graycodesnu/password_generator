cb52e8c62b94b1b800c663b734ee330dbbc0801b
Grayson Harvey <graycodes.nu@gmail.com>
2022-11-12 23:43:04 -0600

    uppercase function working, but not lowercase

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 //! Elements by ID
 var generateBtn = document.querySelector("#generate");
 
-// var passwordTextEl = document.querySelector("#password");
-// var lengthInputEl = document.querySelector("#length");
-// var uppercaseCheckBoxEl = document.querySelector("#uppercase");
-// var lowercaseCheckBoxEl = document.querySelector("#lowercase");
-// var numbersCheckBoxEl = document.querySelector("#numbers");
-// var symbolsCheckBoxEl = document.querySelector("#symbols");
-
-
-// document.getElementById("password").innerHTML = password
-
 //! Define valid characters and requirements
   var getUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var getLowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -34,72 +24,39 @@ function writePassword() {
     );
   }
 
+  //! Uppercase prompt 
   var uppercaseReq = window.confirm("Should uppercase characters be included?");
   if (uppercaseReq) {
     allValidCharacters += getUppercase;
   };
+  console.log("UPPERCASE REQS", getUppercase);
+
+  //! Lowercase prompt
+  var lowercaseReq = window.confirm("Should lowercase characters be included?");
+    if (lowercaseReq) {
+      allValidCharacters += getLowercase;
+    };
+    console.log("LOWERCASE REQS", getLowercase);
 
   }
 
-//   // Uppercase function
-//   var getUppercase = uppercaseCheckBoxEl;
-//   var uppercaseCheckBoxEl = document.getElementById("uppercase");
-//   if (uppercaseCheckBoxEl.checked) {
-//     console.log("yes uppercase");
-//     // return Math.floor(Math.random(getUppercase));
-//   } else {
-//     console.log("no uppercase");
-//   }
-
-//   // Lowercase function
-//   var getLowercase = lowercaseCheckBoxEl;
-//   var lowercaseCheckBoxEl = document.getElementById("lowercase");
-//   if (lowercaseCheckBoxEl.checked) {
-//     console.log("yes lowercase");
-//     // return Math.floor(Math.random(getLowercase));
-//   } else {
-//     console.log("no lowercase");
-//   }
-
-//   // Symbols function
-//   var getSymbols = symbolsCheckBoxEl;
-//   var symbolsCheckBoxEl = document.getElementById("symbols");
-//   if (symbolsCheckBoxEl.checked) {
-//     console.log("yes symbols");
-//     // return Math.floor(Math.random(getSymbols));
-//   } else {
-//     console.log("no symbols");
-//   }
-
-//   // Numbers function
-//   var getNumbers = numbersCheckBoxEl;
-//   var numbersCheckBoxEl = document.getElementById("numbers");
-//   if (numbersCheckBoxEl.checked) {
-//     console.log("yes numbers");
-//     // return Math.floor(Math.random(getNumbers));
-//   } else {
-//     console.log("no numbers");
-//   }
-
-// // Will write to password
-//   // Select el >>> .innerHTML = value
-//   passwordTextEl.innerHTML = "";
-
-//   return generatePassword();
-
-// }
-
-// Will write to password text element 
-
+//! Generate password from requirements using math.floor and math.random
 function generatePassword() {
-  const password = writePassword();
-  var passwordText = document.querySelector("#password");
+  writePassword();
+  for (let i = 1; i <= passwordLength; i++) {
+    let number = Math.floor(Math.random() * allValidCharacters.length)
+    password += allValidCharacters[number];
+  }
+  return password;
+}
+
+//! Display password text
+function displayPassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password")
+
   passwordText.value = password;
-    // For loop
-    for (let i = 0; i < lengthInputValue; i ++) {
-      console.log("running for loop");
-    }
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", displayPassword);
